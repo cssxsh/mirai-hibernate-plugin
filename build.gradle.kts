9184a1bb1361be35e4d54850fe8415ae2dca3b81
cssxsh <cssxsh@gmail.com>
2021-12-26 02:27:12 +0800

    build: 1.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "xyz.cssxsh.mirai"
-version = "1.0.0"
+version = "1.0.1"
 
 
 mavenCentralPublish {
--- src/test/kotlin/xyz/cssxsh/mirai/plugin/MiraiHibernatePluginTest.kt
+++ src/test/kotlin/xyz/cssxsh/mirai/plugin/MiraiHibernatePluginTest.kt
@@ -1,12 +1,27 @@
 package xyz.cssxsh.mirai.plugin.xyz.cssxsh.mirai.plugin
 
+import kotlinx.coroutines.*
 import net.mamoe.mirai.console.plugin.jvm.*
+import net.mamoe.mirai.event.*
+import net.mamoe.mirai.message.data.MessageSource.Key.recall
 import xyz.cssxsh.mirai.plugin.*
 
-class MiraiHibernatePluginTest :
-    KotlinPlugin(JvmPluginDescription(id = "mirai-hibernate-test", name = "mirai-hibernate-test", version = "0.0.0") {
+/**
+ * 也可以不注解 [MiraiHibernate]，这时 [loader] 会自动生成
+ * @see [MiraiHibernateLoader]
+ */
+@MiraiHibernate(loader = MiraiHibernatePluginTest.Loader::class)
+object MiraiHibernatePluginTest : KotlinPlugin(
+    JvmPluginDescription(
+        id = "mirai-hibernate-test",
+        name = "mirai-hibernate-test",
+        version = "0.0.0"
+    ) {
         dependsOn("xyz.cssxsh.mirai.plugin.mirai-hibernate-plugin", false)
-    }) {
+    }
+) {
+
+    object Loader : MiraiHibernateLoader by MiraiHibernateLoader.Impl(plugin = this)
 
     override fun onEnable() {
 
@@ -19,5 +34,20 @@ class MiraiHibernatePluginTest :
         }
 
         println(metadata.driverName)
+
+        globalEventChannel().subscribeMessages {
+            startsWith("撤销") {
+                delay(10_000)
+
+                val source = MiraiHibernateRecorder[source].single().toMessageSource()
+
+                /**
+                 * 这里的原消息内容来自 [xyz.cssxsh.mirai.plugin.entry.MessageRecord.code] 反序列化的结果
+                 */
+                println(source.originalMessage)
+
+                source.recall()
+            }
+        }
     }
 }
