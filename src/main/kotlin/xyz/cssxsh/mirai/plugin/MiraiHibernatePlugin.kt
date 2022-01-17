074fcd38e23c71d552c0bfc8f4a9361d555d46c9
cssxsh <cssxsh@gmail.com>
2022-01-17 16:00:21 +0800

    fix: logger

--- src/main/kotlin/xyz/cssxsh/mirai/plugin/MiraiHibernatePlugin.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/plugin/MiraiHibernatePlugin.kt
@@ -1,8 +1,8 @@
 package xyz.cssxsh.mirai.plugin
 
+import net.mamoe.mirai.console.extension.*
 import net.mamoe.mirai.console.plugin.jvm.*
-import net.mamoe.mirai.event.globalEventChannel
-import net.mamoe.mirai.event.registerTo
+import net.mamoe.mirai.event.*
 import net.mamoe.mirai.utils.*
 
 object MiraiHibernatePlugin : KotlinPlugin(
@@ -12,8 +12,21 @@ object MiraiHibernatePlugin : KotlinPlugin(
         version = "1.0.2",
     ) {
         author("cssxsh")
+
+        dependsOn("net.mamoe.mirai.mirai-slf4j-bridge", true)
     }
 ) {
+    override fun PluginComponentStorage.onLoad() {
+        /**
+         * @see [com.mchange.v2.log.MLogClasses.SLF4J_CNAME]
+         */
+        System.setProperty("com.mchange.v2.log.MLog", "com.mchange.v2.log.slf4j.Slf4jMLog")
+        /**
+         * @see [org.jboss.logging.LoggerProviders.LOGGING_PROVIDER_KEY]
+         */
+        System.setProperty("org.jboss.logging.provider", "slf4j")
+    }
+
     override fun onEnable() {
 
         MiraiHibernateRecorder.registerTo(globalEventChannel())
