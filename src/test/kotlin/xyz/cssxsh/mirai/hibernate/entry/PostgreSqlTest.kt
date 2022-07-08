9a048d80fc6ef0df15287514297581df74bce9cc
cssxsh <cssxsh@gmail.com>
2022-07-08 14:16:23 +0800

    add: postgresql

--- src/test/kotlin/xyz/cssxsh/mirai/hibernate/entry/PostgreSqlTest.kt
+++ src/test/kotlin/xyz/cssxsh/mirai/hibernate/entry/PostgreSqlTest.kt
@@ -1,11 +1,13 @@
 package xyz.cssxsh.mirai.hibernate.entry
 
+import xyz.cssxsh.hibernate.*
 import java.io.File
 
 class PostgreSqlTest : DatabaseTest() {
     init {
         configuration.apply {
             File("./example/postgresql.hibernate.properties").inputStream().use(properties::load)
+            addRandFunction()
         }
     }
 }
