6e2dd3fdb0c43fbf77de9309be9ac8e0f710bb7c
cssxsh <cssxsh@gmail.com>
2022-07-11 19:08:08 +0800

    fix: H2 URL

--- src/main/kotlin/xyz/cssxsh/mirai/hibernate/MiraiHibernateLoader.kt
+++ src/main/kotlin/xyz/cssxsh/mirai/hibernate/MiraiHibernateLoader.kt
@@ -65,7 +65,7 @@ public interface MiraiHibernateLoader {
             classLoader = plugin::class.java.classLoader,
             configuration = plugin.configFolder.resolve("hibernate.properties"),
             default = """
-                hibernate.connection.url=jdbc:h2:${plugin.database("hibernate.h2")}
+                hibernate.connection.url=jdbc:h2:file:${plugin.database("hibernate.h2")}
                 hibernate.connection.driver_class=org.h2.Driver
                 hibernate.dialect=org.hibernate.dialect.H2Dialect
                 hibernate.connection.provider_class=org.hibernate.hikaricp.internal.HikariCPConnectionProvider
