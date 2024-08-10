628025b6e2cdc61be957c31117b608a6e7a057c6
cssxsh <cssxsh@gmail.com>
2024-08-10 16:55:44 +0800

    update: dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,15 +28,15 @@ repositories {
 
 dependencies {
     api("com.zaxxer:HikariCP:5.1.0")
-    api("com.h2database:h2:2.2.224")
-    api("org.xerial:sqlite-jdbc:3.45.3.0")
-    api("com.mysql:mysql-connector-j:8.3.0")
-    api("org.mariadb.jdbc:mariadb-java-client:3.3.3")
+    api("com.h2database:h2:2.3.230")
+    api("org.xerial:sqlite-jdbc:3.46.0.1")
+    api("com.mysql:mysql-connector-j:9.0.0")
+    api("org.mariadb.jdbc:mariadb-java-client:3.4.1")
     api("org.postgresql:postgresql:42.7.3")
     api("org.reflections:reflections:0.10.2")
-    compileOnly("xyz.cssxsh.mirai:mirai-administrator:1.4.0")
+    compileOnly("xyz.cssxsh.mirai:mirai-administrator:1.4.3")
     testImplementation(kotlin("test"))
-    testImplementation("com.microsoft.sqlserver:mssql-jdbc:12.6.1.jre11")
+    testImplementation("com.microsoft.sqlserver:mssql-jdbc:12.8.0.jre11")
     //
     implementation(platform("net.mamoe:mirai-bom:2.16.0"))
     compileOnly("net.mamoe:mirai-core")
@@ -46,12 +46,12 @@ dependencies {
     testImplementation("net.mamoe:mirai-core-utils")
     testImplementation("net.mamoe:mirai-console-compiler-common")
     //
-    implementation(platform("org.hibernate.orm:hibernate-platform:6.4.8.Final"))
+    implementation(platform("org.hibernate.orm:hibernate-platform:6.6.0.Final"))
     api("org.hibernate.orm:hibernate-core")
     api("org.hibernate.orm:hibernate-hikaricp")
     api("org.hibernate.orm:hibernate-community-dialects")
     //
-    implementation(platform("org.slf4j:slf4j-parent:2.0.13"))
+    implementation(platform("org.slf4j:slf4j-parent:2.0.15"))
     testImplementation("org.slf4j:slf4j-simple")
 }
 
